37b8cfa33b00187625d4ee9eb1e10ad84052964e
Jacob Bare <jacob@limit0.io>
2018-01-22 13:00:24 -0600

    Add mongoose and graphql/apollo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+const bluebird = require('bluebird');
+const mongoose = require('mongoose');
 const pkg = require('../package.json');
 
 const app = express();
 const port = process.env.PORT || 8100;
+const dsn = process.env.MONGO_DSN || 'mongodb://localhost:27017/fortnight';
+
+// Initialize DB
+mongoose.Promise = bluebird;
+mongoose.connect(dsn, {
+  ignoreUndefined: true,
+  promiseLibrary: bluebird,
+}).then(() => process.stdout.write(`Mongoose connected to '${dsn}'\n`));
 
 // Global middlewares.
 app.use(helmet());
@@ -13,4 +23,4 @@ app.get('/ping', (req, res) => {
 });
 
 app.listen(port);
-process.stdout.write(`${pkg.name} graph listening on port ${port}\n`);
+process.stdout.write(`Express app '${pkg.name}' listening on port ${port}\n`);
