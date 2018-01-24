e2871ce333a9532cc8246121969cb4d3a62931be
Jacob Bare <jacob@limit0.io>
2018-01-24 08:21:48 -0600

    Do not assume values for process.env values

--- src/index.js
+++ src/index.js
@@ -10,18 +10,16 @@ const pkg = require('../package.json');
 const loadRouters = require('./routers');
 
 const app = express();
-const port = process.env.PORT || 8100;
-const mongoDSN = process.env.MONGO_DSN || 'mongodb://localhost:27017/fortnight';
-const redisDSN = process.env.REDIS_DSN || 'redis://localhost:6379/0';
+const { PORT, MONGO_DSN, REDIS_DSN } = process.env;
 
 // Initialize DB
 mongoose.Promise = bluebird;
-mongoose.connect(mongoDSN, {
+mongoose.connect(MONGO_DSN, {
   ignoreUndefined: true,
   promiseLibrary: bluebird,
-}).then(() => process.stdout.write(`Mongoose connected to '${mongoDSN}'\n`));
+}).then(() => process.stdout.write(`Mongoose connected to '${MONGO_DSN}'\n`));
 
-redis.use('session', { url: redisDSN, prefix: `${pkg.name}:session:` });
+redis.use('session', { url: REDIS_DSN, prefix: `${pkg.name}:session:` });
 
 // Global middlewares.
 app.use(helmet());
@@ -34,5 +32,5 @@ app.use(passport.initialize());
 
 loadRouters(app);
 
-app.listen(port);
-process.stdout.write(`Express app '${pkg.name}' listening on port ${port}\n`);
+app.listen(PORT);
+process.stdout.write(`Express app '${pkg.name}' listening on port ${PORT}\n`);
