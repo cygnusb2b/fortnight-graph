f5266146db52421b64efc40f5e9be509ec78e724
Jacob Bare <jacob@limit0.io>
2018-02-01 14:59:58 -0600

    Complete advertiser repo tests

--- test/repositories/advertiser.spec.js
+++ test/repositories/advertiser.spec.js
@@ -3,6 +3,7 @@ const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 const Repo = require('../../src/repositories/advertiser');
 const Model = require('../../src/models/advertiser');
+const Pagination = require('../../src/classes/pagination');
 const Promise = require('bluebird');
 
 chai.use(chaiAsPromised);
@@ -42,5 +43,70 @@ describe('repositories/advertiser', function() {
       ;
       sinon.assert.calledWith(Model.findOne, { _id: id });
     });
+  });
+  describe('#update', function() {
+    beforeEach(function() {
+      sandbox.stub(Model, 'findOneAndUpdate').callsFake(({ _id }, { $set }, options) => {
+        const val = _id === '1234' ? { _id, name: $set.name } : null;
+        return Promise.resolve(val);
+      });
+
+    });
+    afterEach(function() {
+      sinon.assert.calledOnce(Model.findOneAndUpdate);
+      sandbox.restore();
+    });
+
+    it('should return the updated advertiser document.', async () => {
+      const id = '1234';
+      const name = 'New Name';
+      await expect(Repo.update({ id, name }))
+        .to.be.fulfilled
+        .and.become({ _id: id, name });
+      ;
+      sinon.assert.calledWith(Model.findOneAndUpdate, { _id: id }, { $set: { name } }, { new: true });
+    });
+    it('should error when the advertiser is not found.', async () => {
+      const id = '12345';
+      const name = 'New Name';
+      await expect(Repo.update({ id, name }))
+        .to.be.rejectedWith(Error, `No advertiser found for id '${id}'`)
+      ;
+      sinon.assert.calledWith(Model.findOneAndUpdate, { _id: id }, { $set: { name } }, { new: true });
+    });
+  });
+  describe('#create', function() {
+    beforeEach(function() {
+      sandbox.stub(Model.prototype, 'save').callsFake(() => {
+        return Promise.resolve({ id: '1234' });
+      });
+
+    });
+    afterEach(function() {
+      sinon.assert.calledOnce(Model.prototype.save);
+      sandbox.restore();
+    });
+
+    it('should return the created advertiser document.', async () => {
+      const name = 'New Name';
+      await expect(Repo.create({ name }))
+        .to.be.fulfilled
+        .and.become({ id: '1234' });
+      ;
+    });
+    it('should return the created advertiser document when no payload is present.', async () => {
+      await expect(Repo.create())
+        .to.be.fulfilled
+        .and.become({ id: '1234' });
+      ;
+    });
+  });
+  describe('#paginate', function() {
+    it('should return a Pagination instance.', function(done) {
+      const paginated = Repo.paginate();
+      expect(paginated).to.be.an.instanceOf(Pagination);
+      expect(paginated.Model).to.be.a('function');
+      done();
+    })
   });
 });
