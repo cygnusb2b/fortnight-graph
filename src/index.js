7833d290eb833a6239a6b85608f73330a5d6b7bc
Jacob Bare <jacob@limit0.io>
2018-02-05 14:24:42 -0600

    Move mongoose init to own file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 require('dotenv').config();
 
-const bluebird = require('bluebird');
-const mongoose = require('mongoose');
+const initDb = require('./db');
 const redis = require('./redis');
 const pkg = require('../package.json');
 const app = require('./app');
 
-const {
-  PORT,
-  MONGO_DSN,
-  REDIS_DSN,
-  MONGOOSE_DEBUG,
-} = process.env;
+const { PORT, REDIS_DSN } = process.env;
 
-// Initialize DB
-mongoose.set('debug', Boolean(MONGOOSE_DEBUG));
-mongoose.Promise = bluebird;
-mongoose.connect(MONGO_DSN, {
-  // autoIndex: process.env.NODE_ENV !== 'production',
-  ignoreUndefined: true,
-  promiseLibrary: bluebird,
-}).then(() => process.stdout.write(`Mongoose connected to '${MONGO_DSN}'\n`));
+initDb();
 
 redis.use('session', { url: REDIS_DSN, prefix: `${pkg.name}:session:` });
 
