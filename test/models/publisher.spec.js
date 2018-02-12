9416e1358796e5e9a52b7e5464a1a15efd3fadf4
Jacob Bare <jacob@limit0.io>
2018-02-12 09:42:35 -0600

    Add publisher model test

---
+++ test/models/publisher.spec.js
@@ -0,0 +1,43 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const Publisher = require('../../src/models/publisher');
+const fixtures = require('../../src/fixtures');
+const { testTrimmedField, testUniqueField, testRequiredField } = require('../utils');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const generatePublisher = () => fixtures(Publisher, 1).one();
+
+describe('models/publisher', function() {
+  before(function() {
+    return Publisher.remove();
+  });
+  after(function() {
+    return Publisher.remove();
+  });
+  it('should successfully save.', async function() {
+    const publisher = generatePublisher();
+    await expect(publisher.save()).to.be.fulfilled;
+  });
+
+  describe('#name', function() {
+    let publisher;
+    beforeEach(function() {
+      publisher = generatePublisher();
+    });
+    it('should be trimmed.', function() {
+      return testTrimmedField(Publisher, publisher, 'name');
+    });
+    const values = ['', null, undefined];
+    values.forEach((value) => {
+      it(`should be required and be rejected when the value is '${value}'`, function() {
+        return testRequiredField(Publisher, publisher, 'name', value);
+      });
+    });
+    it('should be unique.', function() {
+      const another = generatePublisher();
+      return testUniqueField(Publisher, publisher, another, 'name');
+    });
+  });
+});
