2681a6ff2b2de76297d65ff65ac5b179e704b866
Jacob Bare <jacob@limit0.io>
2018-02-13 10:55:10 -0600

    Remove commnted code

--- test/connect.js
+++ test/connect.js
@@ -28,24 +28,6 @@ const disconnect = () => Promise.all([
   }),
 ]);
 
-// const disconnect = Promise.all([
-//   new Promise((resolve, reject) => {
-//     const mongoose = require('../src/mongoose');
-//     mongoose.connection.on('disconnected', resolve);
-//     mongoose.disconnect();
-//   }),
-//   new Promise((resolve, reject) => {
-
-//     redis.on('end', () => {
-//       resolve();
-//     });
-//     redis.on('error', (err) => {
-//       reject(err);
-//     });
-//     redis.quit();
-//   }),
-// ]);
-
 before(async function() {
   await connect();
 });
