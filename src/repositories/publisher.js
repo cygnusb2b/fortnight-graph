c28fa743978e1a94b330e41784d75e1c74e1e472
Jacob Bare <jacob@limit0.io>
2018-02-14 11:27:15 -0600

    Add publisher repo

---
+++ src/repositories/publisher.js
@@ -0,0 +1,70 @@
+const Promise = require('bluebird');
+const Publisher = require('../models/publisher');
+const fixtures = require('../fixtures');
+
+module.exports = {
+  /**
+   *
+   * @param {object} payload
+   * @return {Promise}
+   */
+  create(payload = {}) {
+    const publisher = new Publisher(payload);
+    return publisher.save();
+  },
+
+  /**
+   * Find a Publisher record by ID.
+   *
+   * Will return a rejected promise if no ID was provided.
+   * Will NOT reject the promise if the record cannnot be found.
+   *
+   * @param {string} id
+   * @return {Promise}
+   */
+  findById(id) {
+    if (!id) return Promise.reject(new Error('Unable to find publisher: no ID was provided.'));
+    return Publisher.findOne({ _id: id });
+  },
+
+  /**
+   * @param {object} criteria
+   * @return {Promise}
+   */
+  find(criteria) {
+    return Publisher.find(criteria);
+  },
+
+  /**
+   * @todo Should this do a cascade removal?
+   * @param {string} id
+   * @return {Promise}
+   */
+  removeById(id) {
+    if (!id) return Promise.reject(new Error('Unable to remove publisher: no ID was provided.'));
+    return this.remove({ _id: id });
+  },
+
+  /**
+   * @param {object} criteria
+   * @return {Promise}
+   */
+  remove(criteria) {
+    return Publisher.remove(criteria);
+  },
+
+  /**
+   *
+   * @param {number} [count=1]
+   * @return {object}
+   */
+  generate(count = 1) {
+    return fixtures(Publisher, count);
+  },
+
+  async seed({ count = 1 } = {}) {
+    const results = this.generate(count);
+    await Promise.all(results.all().map(model => model.save));
+    return results;
+  },
+};
