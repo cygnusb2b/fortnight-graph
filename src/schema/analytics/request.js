041bf2b932de00147cbc3f404296e7ccb6169865
Jacob Bare <jacob@limit0.io>
2018-03-01 15:35:46 -0600

    Add hashing and aggregate save to request

--- src/schema/analytics/request.js
+++ src/schema/analytics/request.js
@@ -1,5 +1,5 @@
 const { Schema } = require('mongoose');
-// const objectHash = require('object-hash');
+const hash = require('object-hash');
 const CampaignPlacementRepo = require('../../repositories/campaign/placement');
 
 const schema = new Schema({
@@ -15,12 +15,18 @@ const schema = new Schema({
   hour: {
     type: Date,
     required: true,
+    default: () => new Date(),
     set: (v) => {
       v.setMilliseconds(0);
       v.setSeconds(0);
       v.setMinutes(0);
       return v;
     },
+  },
+  last: {
+    type: Date,
+    required: true,
+    default: () => new Date(),
   },
   kv: {
     type: Schema.Types.Mixed,
@@ -33,6 +39,34 @@ const schema = new Schema({
   },
 });
 
-schema.index()
+schema.index({ hash: 1, hour: 1 }, { unique: true });
+
+schema.virtual('hashObj').get(function getToHash() {
+  return {
+    pid: this.pid ? this.pid.toString() : null,
+    kv: this.kv || {},
+  };
+});
+
+schema.methods.buildHash = function createHash() {
+  this.hash = hash(this.hashObj, { algorithm: 'md5' });
+  return this;
+};
+
+schema.methods.aggregateSave = async function aggregateSave() {
+  this.buildHash();
+  await this.validate();
+  const $setOnInsert = {
+    hash: this.hash,
+    pid: this.pid,
+    kv: this.kv,
+    hour: this.hour,
+  };
+  const $set = { last: new Date() };
+  const $inc = { n: 1 };
+  const update = { $setOnInsert, $set, $inc };
+
+  await this.model('analytics-request').findOneAndUpdate({ hash: this.hash, hour: this.hour }, update, { upsert: true });
+};
 
 module.exports = schema;
--- test/schema/analytics/request.spec.js
+++ test/schema/analytics/request.spec.js
@@ -1,3 +1,4 @@
+require('../../connections');
 const AnalyticsRequest = require('../../../src/models/analytics/request');
 const CampaignPlacementRepo = require('../../../src/repositories/campaign/placement');
 
@@ -5,7 +6,7 @@ const sandbox = sinon.createSandbox();
 
 describe('schema/analytics/request', function() {
 
-  describe('#kv', function() {
+  describe('.kv', function() {
     beforeEach(function() {
       sandbox.spy(CampaignPlacementRepo, 'cleanTargetingVars');
     });
@@ -31,7 +32,7 @@ describe('schema/analytics/request', function() {
     });
   });
 
-  describe('#hour', function() {
+  describe('.hour', function() {
     it('should remove the milli, seconds, and minutes from the date.', function(done) {
       const request = new AnalyticsRequest();
       request.hour = new Date();
@@ -41,6 +42,86 @@ describe('schema/analytics/request', function() {
       expect(request.hour.getMinutes()).to.equal(0);
 
       done();
+    });
+  });
+
+  describe('.hashObj', function() {
+    it('should return the object to hash.', function(done) {
+      const request = new AnalyticsRequest({
+        pid: '5410f52389ce2f8354ac8e2e',
+        kv: { foo: 'bar' },
+        hour: new Date(),
+      });
+      request.kv = { bar: 'foo' };
+
+      expect(request.hashObj).to.deep.equal({
+        pid: '5410f52389ce2f8354ac8e2e',
+        kv: { bar: 'foo' },
+      });
+      done();
+    });
+  });
+
+  describe('#validate', function() {
+    it('should throw an error when the model is invalid.', async function() {
+      const request = new AnalyticsRequest();
+      await expect(request.validate()).to.be.rejectedWith(Error, /validation failed/i);
+    });
+  });
+
+  describe('#buildHash', function() {
+    it('should create the hash and successfully validate.', async function() {
+      const request = new AnalyticsRequest({
+        pid: '5410f52389ce2f8354ac8e2e',
+        kv: { foo: 'bar' },
+        hour: new Date(),
+      });
+      request.buildHash();
+
+      expect(request.hash).to.be.a('string').that.matches(/[a-f0-9]{32}/);
+      await expect(request.validate()).to.be.fulfilled;
+    });
+  });
+
+  describe('#aggregateSave', function() {
+    before(async function() {
+      await AnalyticsRequest.remove();
+    });
+    after(async function() {
+      await AnalyticsRequest.remove();
+    });
+
+    it('should reject when invalid.', async function() {
+      const request = new AnalyticsRequest();
+      await expect(request.aggregateSave()).to.be.rejectedWith(Error, /validation failed/i);
+    });
+
+    it('should save/upsert.', async function() {
+      const date = new Date(1519939126481);
+      const request = new AnalyticsRequest({
+        hour: date,
+        pid: '5410f52389ce2f8354ac8e2e',
+        kv: { foo: 'bar' },
+      });
+      await expect(request.aggregateSave()).to.be.fulfilled;
+      const result = await AnalyticsRequest.findOne({ hash: request.hash });
+      expect(result.hash).to.equal(request.hash);
+      expect(result.n).to.equal(1);
+      expect(result.hour.getMinutes()).to.equal(0);
+      expect(result.last.getMilliseconds()).to.be.gt(0);
+      expect(result.hour.getHours()).to.equal(date.getHours());
+    });
+    it('should save/upsert and increment.', async function() {
+      const date = new Date(1519939126481);
+      const request = new AnalyticsRequest({
+        hour: date,
+        pid: '5410f52389ce2f8354ac8e2e',
+        kv: { foo: 'bar' },
+      });
+      await expect(request.aggregateSave()).to.be.fulfilled;
+      const result = await AnalyticsRequest.findOne({ hash: request.hash });
+      expect(result.hash).to.equal(request.hash);
+      expect(result.n).to.equal(2);
     });
   });
 
