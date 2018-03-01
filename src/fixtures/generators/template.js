f828f5c12a804dd9bb7fd91a8dfaf5364ca9ae22
Jacob Bare <jacob@limit0.io>
2018-03-01 10:44:56 -0600

    Add larger fallback template to generator

--- src/fixtures/generators/template.js
+++ src/fixtures/generators/template.js
@@ -70,10 +70,71 @@ const html = `
 `;
 
 const fallback = `
-<div>
-  <h1>{{ creative.title }}</h1>
-  <p>{{ campaign.createdAt }}</p>
-  <p>{{ c.customVar }}</p>
+{{!-- Load More Template Style --}}
+
+<div class="item" data-bvo-width="33" data-bvo-item-id="{{ contentId }}" data-bvo-item-type="platform-content">
+
+  {{#if image.src}}
+    <div class="element" data-bvo-type="field.image" data-bvo-value-wrap-element="span" data-bvo-linkable="true" data-bvo-width="100"
+      data-bvo-position="left" data-bvo-param-field-key="primaryImage" data-bvo-param-fallback-keys="" data-bvo-param-convert-breaks="false"
+      data-bvo-param-filter="" data-bvo-param-is-fragment="false" data-bvo-param-fragment-url="" data-bvo-param-aspect-ratio="16:9"
+      data-bvo-param-fit="contain" data-bvo-template-name="platform-content.primary-image">
+      <a href="{{ url }}"
+        title="{{ title }}" target="_self">
+        <noscript>
+          <img class="img-responsive" src="{{ image.src }}"
+            alt="{{ image.alt }}" title="{{ image.alt }}" style="opacity:1;">
+        </noscript>
+        <img id="{{ image.id }}" class="img-responsive" data-bvo-src="{{ image.src }}"
+          alt="{{ image.alt }}" title="{{ image.alt }}">
+      </a>
+    </div>
+  {{/if}}
+
+  <div class="element" data-bvo-type="group" data-bvo-value-wrap-element="span" data-bvo-linkable="false" data-bvo-width="100"
+    data-bvo-position="left">
+    <div class="element" data-bvo-type="field" data-bvo-value-wrap-element="h3" data-bvo-linkable="true" data-bvo-width="100"
+      data-bvo-position="left" data-bvo-param-field-key="shortName" data-bvo-param-fallback-keys="nameWebsite,name" data-bvo-param-convert-breaks="false"
+      data-bvo-param-filter="" data-bvo-param-is-fragment="false" data-bvo-param-fragment-url="" data-bvo-template-name="platform-content.shortName">
+      <a href="{{ url }}"
+        title="{{ title }}" target="_self">
+        <h3 class="value" data-bvo-item-id="{{ contentId }}" data-bvo-item-type="platform-content">
+          {{ title }}
+        </h3>
+      </a>
+    </div>
+
+
+    <div class="element" data-bvo-type="field" data-bvo-value-wrap-element="span" data-bvo-linkable="false" data-bvo-width="100"
+      data-bvo-position="left" data-bvo-param-field-key="teaserWebsite" data-bvo-param-fallback-keys="teaser" data-bvo-param-convert-breaks="false"
+      data-bvo-param-filter="" data-bvo-param-is-fragment="false" data-bvo-param-fragment-url="" data-bvo-template-name="platform-content.teaser">
+      <span class="value" data-bvo-item-id="{{ contentId }}" data-bvo-item-type="platform-content">
+        {{ teaser }}
+      </span>
+    </div>
+
+    <div class="element" data-bvo-type="group" data-bvo-value-wrap-element="span" data-bvo-linkable="false" data-bvo-width="100"
+      data-bvo-position="left" data-bvo-template-name="platform-content.tag+published">
+      <div class="element" data-bvo-type="field" data-bvo-value-wrap-element="span" data-bvo-linkable="true" data-bvo-width="0"
+        data-bvo-position="left" data-bvo-param-field-key="primarySectionWebsite.name" data-bvo-param-fallback-keys="" data-bvo-param-convert-breaks="false"
+        data-bvo-param-filter="" data-bvo-param-is-fragment="false" data-bvo-param-fragment-url="" data-bvo-template-name="platform-content.website-section-tag">
+        <a href="{{ section.url }}" title="{{ section.name }}" target="_self">
+          <span class="value" data-bvo-item-id="{{ section.id }}" data-bvo-item-type="website-section">
+            {{ section.name }}
+          </span>
+        </a>
+      </div>
+
+      <div class="element" data-bvo-type="field.date" data-bvo-value-wrap-element="span" data-bvo-linkable="false" data-bvo-width="0"
+        data-bvo-position="left" data-bvo-param-field-key="published" data-bvo-param-fallback-keys="created" data-bvo-param-convert-breaks="false"
+        data-bvo-param-filter="" data-bvo-param-is-fragment="false" data-bvo-param-fragment-url="" data-bvo-param-format="ap:mdy"
+        data-bvo-template-name="platform-content.published-date">
+        <span class="value" data-bvo-item-id="{{ contentId }}" data-bvo-item-type="platform-content">
+          {{ publishedDate }}
+        </span>
+      </div>
+    </div>
+  </div>
 </div>
 `;
 
