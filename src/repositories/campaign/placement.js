13ff5ad6dbe3cffb2966781db66a20b6e5af6051
Jacob Bare <jacob@limit0.io>
2018-03-01 11:17:49 -0600

    Update placement delivery to render template

--- src/repositories/campaign/placement.js
+++ src/repositories/campaign/placement.js
@@ -1,8 +1,9 @@
 const createError = require('http-errors');
 const Placement = require('../../models/placement');
 const Template = require('../../models/template');
+const TemplateRepo = require('../../repositories/template');
 const Campaign = require('../../models/campaign');
-const Request = require('../../models/request');
+const randomBetween = require('../../utils/random-between');
 
 module.exports = {
   parseOptions(options) {
@@ -48,14 +49,12 @@ module.exports = {
     if (!template) throw createError(404, `No template exists for ID '${templateId}'`);
 
     /**
-     * @todo
-     * The ad selection algo would now run, find the appropriate ad (or ads), and replace
-     * the template's merge variables. Also, replace any custom merge variables. For now, simulate.
+     * For now, merely return the number of requested ads.
+     * Eventually this should use scheduling, weighting, custom variable targeting, etc.
      */
     const campaigns = await Campaign.find().limit(limit);
 
     const ads = [];
-    const reqs = [];
     /**
      * @todo The request tracking implementation *definitely* needs work.
      * Some sort of pre-aggregation should exist.
@@ -63,26 +62,29 @@ module.exports = {
      * Merge variables also need to be stored on the request.
      */
     campaigns.forEach((campaign) => {
-      const cid = campaign.get('id');
-      const request = new Request({ cid, pid: placementId });
-      reqs.push(request);
+      const count = campaign.get('creatives.length');
+      const ad = {
+        campaignId: campaign.id,
+        creativeId: null,
+        html: '',
+      };
 
-      const correlator = this.createCorrelator(requestURL, request.get('id'));
-      const html = template.html
-        .replace(/{{ id }}/g, campaign.get('id'))
-        /**
-         * @todo This needs to use the campaign creative title, not name.
-         */
-        .replace(/{{ title }}/g, campaign.get('name'));
-      ads.push({ name: campaign.name, html: `${html}\n${correlator}` });
+      if (count) {
+        // Rotate the creative randomly. Eventually weighting could be added.
+        const index = randomBetween(0, count - 1);
+        const creative = campaign.get(`creatives.${index}`);
+        // Render the template.
+        // @todo The tracking becon/correlator needs to be appended to the creative.
+        // @todo The click tracker also needs to be added.
+        ad.html = TemplateRepo.render(template.html, { campaign, creative });
+        ad.creativeId = creative.id;
+      }
+      ads.push(ad);
     });
-    if (reqs.length) {
-      await Request.collection.insertMany(reqs);
-    }
     return ads;
   },
 
-  createCorrelator(url, id) {
-    return `<img src="${url}/c/l/${id}.gif" data-view-src="${url}/c/v/${id}.gif">`;
-  },
+  // createCorrelator(url, id) {
+  //   return `<img src="${url}/c/l/${id}.gif" data-view-src="${url}/c/v/${id}.gif">`;
+  // },
 };
