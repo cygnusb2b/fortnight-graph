3b4ef810e4d3315e0035e06d273464a8662d1fdb
Jacob Bare <jacob@limit0.io>
2018-03-07 09:40:15 -0600

    Save load/view events to database

--- src/routers/track.js
+++ src/routers/track.js
@@ -1,30 +1,41 @@
 const jwt = require('jsonwebtoken');
 const { Router } = require('express');
 const { noCache } = require('helmet');
+const AnalyticsLoad = require('../models/analytics/load');
+const AnalyticsView = require('../models/analytics/view');
 
 const emptyGif = Buffer.from('R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', 'base64');
+
 const router = Router();
 router.use(noCache());
 
 const events = ['load', 'view'];
+const modelMap = {
+  load: AnalyticsLoad,
+  view: AnalyticsView,
+};
+
+const send = (res, status, message) => {
+  if (message) res.set('X-Error-Message', message);
+  res.status(status);
+  res.send(emptyGif);
+};
 
 router.get('/:token/:event.gif', (req, res) => {
   res.set('Content-Type', 'image/gif');
   const { token, event } = req.params;
 
-  if (events.includes(event)) {
-    jwt.verify(token, process.env.TRACKER_SECRET, { algorithms: 'HS256' }, (err) => {
-      if (err) {
-        res.status(403);
-      } else {
-        // @todo Save the event.
-        res.status(200);
-      }
-    });
-  } else {
-    res.status(400);
+  if (!events.includes(event)) {
+    return send(res, 400, `The event type '${event}' is invalid.`);
   }
-  res.send(emptyGif);
+  return jwt.verify(token, process.env.TRACKER_SECRET, { algorithms: 'HS256' }, (err, payload) => {
+    if (err) return send(res, 403, err.message);
+    const Model = modelMap[event];
+    const { cid, hash } = payload;
+    const last = new Date();
+    const doc = new Model({ hash, cid, last });
+    return doc.aggregateSave().then(() => send(res, 200)).catch(e => send(res, 500, e.message));
+  });
 });
 
 module.exports = router;
--- test/routers/track.spec.js
+++ test/routers/track.spec.js
@@ -1,6 +1,8 @@
 require('../connections');
 const app = require('../../src/app');
 const CampaignPlacementRepo = require('../../src/repositories/campaign/placement');
+const AnalyticsLoad = require('../../src/models/analytics/load');
+const AnalyticsView = require('../../src/models/analytics/view');
 const router = require('../../src/routers/track');
 
 const emptyGif = Buffer.from('R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', 'base64');
@@ -15,8 +17,19 @@ const testImageResponse = (res) => {
   headers.forEach(header => expect(res.get(header.key)).to.equal(header.value));
   expect(res.body.toString()).to.equal(emptyGif.toString());
 };
+const testErrorHeader = (res) => {
+  expect(res.get('X-Error-Message')).to.be.a('string');
+};
 
 describe('routers/track', function() {
+  before(async function() {
+    await AnalyticsLoad.remove();
+    await AnalyticsView.remove();
+  });
+  after(async function() {
+    await AnalyticsLoad.remove();
+    await AnalyticsView.remove();
+  });
   it('should export a router function.', function(done) {
     expect(router).to.be.a('function');
     expect(router).itself.to.respondTo('use');
@@ -27,6 +40,7 @@ describe('routers/track', function() {
       .get('/t/bad-token-value/bad-event.gif')
       .expect(400)
       .expect(testImageResponse)
+      .expect(testErrorHeader)
       .end(done);
   });
   it('should return a 403 when a bad token is provided.', function(done) {
@@ -34,6 +48,7 @@ describe('routers/track', function() {
       .get('/t/bad-token-value/view.gif')
       .expect(403)
       .expect(testImageResponse)
+      .expect(testErrorHeader)
       .end(done);
   });
   it('should return a 404 if the image extension is missing.', function(done) {
@@ -41,6 +56,24 @@ describe('routers/track', function() {
       .get('/t/token/view')
       .expect(404)
       .end(done);
+  });
+  it('should return a 500 on a bad database operation.', function(done) {
+    const stub = sinon.stub(AnalyticsLoad.prototype, 'aggregateSave').rejects(new Error('Bad stuff!'));
+
+    const campaignId = '5a9db9fb9fb64eb206ddf848';
+    const hash = '01f5c84a826ebc85b8abbe318b400ad3';
+    const endpoint = CampaignPlacementRepo.createTracker('load', campaignId, '', hash);
+    request(app)
+      .get(endpoint)
+      .expect(500)
+      .expect(testImageResponse)
+      .expect(testErrorHeader)
+      .end(() => {
+        sinon.assert.called(stub);
+        stub.restore();
+        done();
+      });
+
   });
   it('should respond to the load event.', function(done) {
     const campaignId = '5a9db9fb9fb64eb206ddf848';
@@ -52,7 +85,27 @@ describe('routers/track', function() {
       .expect(testImageResponse)
       .end(done);
   });
+  it('and upsert another load event.', function(done) {
+    const campaignId = '5a9db9fb9fb64eb206ddf848';
+    const hash = '01f5c84a826ebc85b8abbe318b400ad3';
+    const endpoint = CampaignPlacementRepo.createTracker('load', campaignId, '', hash);
+    request(app)
+      .get(endpoint)
+      .expect(200)
+      .expect(testImageResponse)
+      .end(done);
+  });
   it('should respond to the view event.', function(done) {
+    const campaignId = '5a9db9fb9fb64eb206ddf848';
+    const hash = '01f5c84a826ebc85b8abbe318b400ad3';
+    const endpoint = CampaignPlacementRepo.createTracker('view', campaignId, '', hash);
+    request(app)
+      .get(endpoint)
+      .expect(200)
+      .expect(testImageResponse)
+      .end(done);
+  });
+  it('and upsert another view event.', function(done) {
     const campaignId = '5a9db9fb9fb64eb206ddf848';
     const hash = '01f5c84a826ebc85b8abbe318b400ad3';
     const endpoint = CampaignPlacementRepo.createTracker('view', campaignId, '', hash);
