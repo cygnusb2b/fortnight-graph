ee23a4cc031a190ac73c7e29a18004705e5fd868
Jacob Bare <jacob@limit0.io>
2018-03-07 12:16:03 -0600

    Add the /go redirect router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 const graph = require('./graph');
 const placement = require('./placement');
 const track = require('./track');
+const go = require('./go');
 
 module.exports = (app) => {
   app.use('/graph', graph);
   app.use('/placement', placement);
   app.use('/t', track);
+  app.use('/go', go);
 };
