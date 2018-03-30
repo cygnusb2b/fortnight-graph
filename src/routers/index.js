ded29397b43d0ce3ef59c20313d139bf0db2e88b
Jacob Bare <jacob@limit0.io>
2018-03-30 07:44:38 -0500

    Remove the `/t` track router
    The track router at `/t` is no longer being used and is not receiving any requests. It was originally deprecated by the `/e` event router.

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,5 @@
 const graph = require('./graph');
 const placement = require('./placement');
-const track = require('./track');
 const event = require('./event');
 const redir = require('./redir');
 const go = require('./go');
@@ -8,7 +7,6 @@ const go = require('./go');
 module.exports = (app) => {
   app.use('/graph', graph);
   app.use('/placement', placement);
-  app.use('/t', track);
   app.use('/e', event);
   app.use('/redir', redir);
   app.use('/go', go);
