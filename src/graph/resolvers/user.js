88322ba98b1fadf2f7de2b0fee584253b7c99a7a
Jacob Bare <jacob@limit0.io>
2018-04-26 12:37:47 -0500

    Add change user password and profile

--- src/graph/resolvers/user.js
+++ src/graph/resolvers/user.js
@@ -1,5 +1,12 @@
+const User = require('../../models/user');
 const UserRepo = require('../../repositories/user');
 const SessionRepo = require('../../repositories/session');
+
+const validatePassword = (value, confirm) => {
+  if (!value || !confirm) throw new Error('You must provide and confirm your password.');
+  if (value.length < 6) throw new Error('Passwords must be at least six characters long.');
+  if (value !== confirm) throw new Error('The password does not match the confirmation password.');
+};
 
 module.exports = {
   /**
@@ -45,6 +52,34 @@ module.exports = {
         await SessionRepo.delete(auth.session);
       }
       return 'ok';
+    },
+
+    /**
+     *
+     */
+    changeUserPassword: async (root, { input }, { auth }) => {
+      auth.check();
+      const { user } = auth;
+      const { id, value, confirm } = input;
+      if (user.id.valueOf() === id || auth.isAdmin()) {
+        validatePassword(value, confirm);
+        const record = await User.findOne({ _id: id });
+        if (!record) throw new Error(`No user record found for ID ${id}.`);
+        record.password = value;
+        return record.save();
+      }
+      throw new Error('Only administrators can change passwords for other users.');
+    },
+
+      /**
+     *
+     */
+    updateCurrentUserProfile: async (root, { input }, { auth }) => {
+      auth.check();
+      const { givenName, familyName } = input;
+      const { user } = auth;
+      user.set({ givenName, familyName });
+      return user.save();
     },
   },
 };
