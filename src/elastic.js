18cda9691b83588cca6b20f8d30d23abaff6afd0
Jacob Bare <jacob@limit0.io>
2018-05-14 15:59:33 -0500

    Add throttling to elasticsearch connect

--- src/elastic.js
+++ src/elastic.js
@@ -1,23 +1,20 @@
 const { Client } = require('elasticsearch');
+const { throttle } = require('lodash/function');
 
 const { ELASTIC_HOST } = process.env;
-console.info(ELASTIC_HOST);
 
 const client = new Client({ host: ELASTIC_HOST });
 
-const checkConnection = async () => {
-  let isConnected = false;
-  while (!isConnected) {
-    process.stdout.write(`Waiting to connect to ElasticSearch: '${ELASTIC_HOST}'\n`);
-    try {
-      // eslint-disable-next-line no-await-in-loop
-      const health = await client.cluster.health({});
-      process.stdout.write(`Successfully connected to ElasticSearch: ${JSON.stringify(health)}\n`);
-      isConnected = true;
-    } catch (e) {
-      process.stdout.write('Unable to connect to ElasticSearch. Retrying...');
-    }
+const checkConnection = throttle(async () => {
+  process.stdout.write(`Waiting to connect to ElasticSearch: '${ELASTIC_HOST}'\n`);
+  try {
+    // eslint-disable-next-line no-await-in-loop
+    const health = await client.cluster.health({});
+    process.stdout.write(`Successfully connected to ElasticSearch: ${JSON.stringify(health)}\n`);
+  } catch (e) {
+    process.stdout.write('Unable to connect to ElasticSearch. Retrying...\n');
+    checkConnection();
   }
-};
+}, 3000);
 
 module.exports = { client, checkConnection };
