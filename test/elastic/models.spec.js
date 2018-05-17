eb599f26683f27e7294699d945c0fb6d07239655
Jacob Bare <jacob@limit0.io>
2018-05-17 11:22:00 -0500

    Add elastic models test

---
+++ test/elastic/models.spec.js
@@ -0,0 +1,17 @@
+const models = require('../../src/elastic/models');
+
+describe('elastic/models', function() {
+  it('should return search enabled models.', function(done) {
+    const expected = [
+      'advertiser',
+      'campaign',
+      'publisher',
+      'placement',
+      'contact',
+      'template',
+    ].sort();
+    const names = models.map(Model => Model.modelName);
+    expect(names.sort()).to.deep.equal(expected);
+    done();
+  });
+});
