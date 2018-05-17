468c8408ff3d5b1591657c48a71c271480653ad8
Jacob Bare <jacob@limit0.io>
2018-05-17 10:23:57 -0500

    Require connections and add elastic to server test

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,10 +1,10 @@
+require('./connections');
 const expect = require('chai').expect;
 const server = require('../src/server');
 
 describe('server', function() {
   it('should return an object.', function(done) {
-    expect(server).to.be.an('object');
-    expect(server).to.have.keys(['app', 'redis', 'mongoose']);
+    expect(server).to.be.an('object').with.keys(['app', 'redis', 'mongoose', 'elastic']);
     done();
   });
 });
