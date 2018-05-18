6271d5056617a288e4f533ce7f17df38400d636e
Jacob Bare <jacob@limit0.io>
2018-05-18 08:28:31 -0500

    Add initial advertiser search capability

--- src/graph/resolvers/advertiser.js
+++ src/graph/resolvers/advertiser.js
@@ -3,6 +3,7 @@ const AdvertiserRepo = require('../../repositories/advertiser');
 const ContactRepo = require('../../repositories/contact');
 const Advertiser = require('../../models/advertiser');
 const paginationResolvers = require('./pagination');
+const elastic = require('../../elastic');
 
 module.exports = {
   /**
@@ -49,9 +50,65 @@ module.exports = {
     /**
      *
      */
-    searchAdvertisers: (root, { pagination, search }, { auth }) => {
+    searchAdvertisers: async (root, { pagination, phrase }, { auth }) => {
       auth.check();
-      return AdvertiserRepo.search({ pagination, search });
+
+      // @todo If the phrase matches an ID, return the model as an array.
+      const { index, type } = Advertiser.esOptions();
+      const query = {
+        bool: {
+          should: [
+            { match: { 'name.exact': { query: phrase, boost: 10 } } },
+            { match: { name: { query: phrase, operator: 'and', boost: 5 } } },
+            { match: { 'name.phonetic': { query: phrase, boost: 3 } } },
+            { match: { 'name.edge': { query: phrase, operator: 'and', boost: 2 } } },
+            { match: { 'name.edge': { query: phrase, boost: 1 } } },
+            { match: { 'name.ngram': { query: phrase, operator: 'and', boost: 0.5 } } },
+            { match: { 'name.ngram': { query: phrase } } },
+          ],
+        },
+      };
+
+      const totalCount = await elastic.count(index, type, { query });
+
+      const { first, after } = pagination;
+      const results = await elastic.search(index, type, {
+        size: first || 20,
+        query,
+        search_after: after || undefined,
+        sort: [
+          { _score: 'desc' },
+          { _id: 'desc' },
+        ],
+      }, { searchType: 'dfs_query_then_fetch' });
+
+      const { total, hits } = results.hits;
+
+      const mapped = hits.reduce((obj, hit) => ({
+        ...obj,
+        [hit._id]: {
+          score: hit._score, // eslint-disable-line no-underscore-dangle
+          sort: hit.sort,
+        },
+      }), {});
+
+      const mapVal = (doc, prop) => mapped[doc._id.toString()][prop];
+
+      const advertisers = await Advertiser.find({ _id: { $in: Object.keys(mapped) } });
+      const sorted = advertisers.sort((a, b) => mapVal(b, 'score') - mapVal(a, 'score'));
+
+      return {
+        getTotalCount: () => totalCount.count,
+        getEdges: () => sorted.map(node => ({
+          node,
+          cursor: mapVal(node, 'sort'),
+        })),
+        hasNextPage: () => (total >= totalCount.count),
+        getEndCursor: () => {
+          const last = hits[total - 1];
+          return last ? JSON.stringify(last.sort) : null;
+        },
+      };
     },
   },
 
