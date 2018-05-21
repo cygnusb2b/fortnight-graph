f64daa836d5404472fd04bfba55b59fc0f7b49f4
Jacob Bare <jacob@limit0.io>
2018-05-21 11:53:51 -0500

    Move common query definitions and create multi field

--- src/elastic/utils.js
+++ src/elastic/utils.js
@@ -2,6 +2,30 @@ const Pagination = require('../classes/pagination');
 const SearchPagination = require('../classes/elastic/pagination');
 const { client } = require('./index');
 
+const entityNameDefinitions = {
+  exact: {
+    suffix: 'exact', props: { boost: 10 },
+  },
+  root: {
+    suffix: null, props: { operator: 'and', boost: 5 },
+  },
+  phonetic: {
+    suffix: 'phonetic', props: { boost: 3 },
+  },
+  edgeAnd: {
+    suffix: 'edge', props: { operator: 'and', boost: 2 },
+  },
+  edgeOr: {
+    suffix: 'edge', props: { boost: 1 },
+  },
+  ngramAnd: {
+    suffix: 'ngram', props: { operator: 'and', boost: 0.5 },
+  },
+  ngramOr: {
+    suffix: 'ngram', props: {},
+  },
+};
+
 module.exports = {
   /**
    *
@@ -25,20 +49,35 @@ module.exports = {
     return new SearchPagination(Model, client, { params, pagination });
   },
 
-  buildEntityNameQuery(query) {
-    return {
-      bool: {
-        should: [
-          { match: { 'name.exact': { query, boost: 10 } } },
-          { match: { name: { query, operator: 'and', boost: 5 } } },
-          { match: { 'name.phonetic': { query, boost: 3 } } },
-          { match: { 'name.edge': { query, operator: 'and', boost: 2 } } },
-          { match: { 'name.edge': { query, boost: 1 } } },
-          { match: { 'name.ngram': { query, operator: 'and', boost: 0.5 } } },
-          { match: { 'name.ngram': { query } } },
-        ],
-      },
-    };
+  buildEntityNameQuery(query, fieldName = 'name') {
+    const should = [];
+    Object.keys(entityNameDefinitions).forEach((name) => {
+      const def = entityNameDefinitions[name];
+
+      const { suffix, props } = def;
+      props.query = query;
+
+      const field = suffix ? `${fieldName}.${suffix}` : fieldName;
+      should.push({ match: { [field]: props } });
+    });
+    return { bool: { should } };
+  },
+
+  buildMultipleEntityNameQuery(query, fieldNames) {
+    const should = [];
+    Object.keys(entityNameDefinitions).forEach((name) => {
+      const def = entityNameDefinitions[name];
+      const { suffix, props } = def;
+      const match = {
+        ...props,
+        query,
+        type: 'cross_fields',
+        fields: fieldNames.map(field => (suffix ? `${field}.${suffix}` : field)),
+      };
+      should.push({ multi_match: match });
+    });
+    console.dir(should, { depth: 5 });
+    return { bool: { should } };
   },
 
   buildEntityAutocomplete(query) {
