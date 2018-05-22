61583530e711c407bf1381b0e173457282c51960
Josh Worden <solocommand@gmail.com>
2018-05-22 09:48:57 -0500

    Add check for missing prop

--- src/email-templates/index.js
+++ src/email-templates/index.js
@@ -5,6 +5,7 @@ const templates = {};
 
 module.exports = {
   render(key, data = {}) {
+    if (!key) throw new Error('"key" parameter is required.');
     if (!templates[key]) {
       const html = fs.readFileSync(`src/email-templates/${key}.hbs`, 'utf8');
       templates[key] = handlebars.compile(html);
