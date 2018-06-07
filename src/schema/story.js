dd71224e37dde83cfb08a3c19d16482ab8409766
Jacob Bare <jacob@limit0.io>
2018-06-07 14:12:00 -0500

    Add advertiser id and name to story

--- src/schema/story.js
+++ src/schema/story.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const connection = require('../mongoose');
 
 const schema = new Schema({
   title: {
@@ -14,6 +15,28 @@ const schema = new Schema({
     type: String,
     trim: true,
   },
+  advertiserId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('advertiser').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No advertiser found for ID {VALUE}',
+    },
+  },
+  advertiserName: {
+    type: String,
+  },
 }, { timestamps: true });
+
+schema.pre('save', async function setAdvertiserName() {
+  if (this.isModified('advertiserId') || !this.advertiserName) {
+    const advertiser = await connection.model('advertiser').findOne({ _id: this.advertiserId }, { name: 1 });
+    this.advertiserName = advertiser.name;
+  }
+});
 
 module.exports = schema;
