d1a2297c1bb3af568c92918f278754727e21ff68
Jacob Bare <jacob@limit0.io>
2018-06-18 14:11:43 -0500

    Add account settings schema

--- src/schema/account.js
+++ src/schema/account.js
@@ -1,5 +1,43 @@
 const { Schema } = require('mongoose');
 const slug = require('slug');
+const uuid = require('uuid/v4');
+const pushId = require('unique-push-id');
+
+const sessionSchema = new Schema({
+  globalSecret: {
+    type: String,
+    default() {
+      return `${pushId()}.${uuid()}`;
+    },
+  },
+  namespace: {
+    type: String,
+    default() {
+      return uuid();
+    },
+  },
+  expiration: {
+    type: Number,
+    default: 86400,
+    min: 10,
+    max: 31536000,
+  },
+});
+
+const settingsSchema = new Schema({
+  cname: {
+    type: String,
+  },
+  bcc: {
+    type: String,
+  },
+  session: {
+    type: sessionSchema,
+    default() {
+      return {};
+    },
+  },
+});
 
 const schema = new Schema({
   name: {
@@ -14,6 +52,12 @@ const schema = new Schema({
     unique: true,
     set(v) {
       return slug(v);
+    },
+  },
+  settings: {
+    type: settingsSchema,
+    default() {
+      return {};
     },
   },
 }, { timestamps: true });
