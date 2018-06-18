502266ea31adbb193ce5474851a5b7e2e2b6a844
Jacob Bare <jacob@limit0.io>
2018-06-18 13:49:37 -0500

    Generate the instance DSN based on core

--- src/connections/mongoose/instance.js
+++ src/connections/mongoose/instance.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 const output = require('../../output');
 
-const { MONGO_INSTANCE_DSN, MONGOOSE_DEBUG } = process.env;
+const { MONGO_DSN, MONGOOSE_DEBUG, ACCOUNT_KEY } = process.env;
 mongoose.set('debug', Boolean(MONGOOSE_DEBUG));
 mongoose.Promise = bluebird;
 
-const connection = mongoose.createConnection(MONGO_INSTANCE_DSN, {
+const instanceDSN = MONGO_DSN.replace('/fortnight', `/fortnight-${ACCOUNT_KEY}`);
+
+const connection = mongoose.createConnection(instanceDSN, {
   // autoIndex: process.env.NODE_ENV !== 'production',
   ignoreUndefined: true,
   promiseLibrary: bluebird,
 });
-connection.once('open', () => output.write(`🛢️ 🛢️ 🛢️ Successful INSTANCE MongoDB connection to '${MONGO_INSTANCE_DSN}'`));
+connection.once('open', () => output.write(`🛢️ 🛢️ 🛢️ Successful INSTANCE MongoDB connection to '${instanceDSN}'`));
 module.exports = connection;
