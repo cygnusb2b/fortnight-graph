db970a10f780297ac6a7bb8ecd4d87f712e1a4f4
Josh Worden <solocommand@gmail.com>
2018-07-10 09:57:05 -0500

    Add cron job to root

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const env = require('./env');
 const output = require('./output');
 const pkg = require('../package.json');
 const { app } = require('./server');
+const contactNotifier = require('./services/contact-notifier');
 
 const { PORT } = env;
 
 const server = app.listen(PORT);
 output.write(`🕸️ 🕸️ 🕸️ Express app '${pkg.name}' listening on port ${PORT}`);
+
+contactNotifier.init();
 
 module.exports = server;
