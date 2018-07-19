3ca4d1f9465676ffff5197792cd340330fe3be1f
Jacob Bare <jacob@limit0.io>
2018-07-19 09:46:21 -0500

    Add templates and topics to placement schema

--- src/schema/placement.js
+++ src/schema/placement.js
@@ -5,8 +5,16 @@ const { applyElasticPlugin, setEntityFields } = require('../elastic/mongoose');
 const schema = new Schema({
   name: {
     type: String,
-    required: true,
     trim: true,
+  },
+  publisherName: {
+    type: String,
+  },
+  topicName: {
+    type: String,
+  },
+  templateName: {
+    type: String,
   },
   publisherId: {
     type: Schema.Types.ObjectId,
@@ -20,8 +28,28 @@ const schema = new Schema({
       message: 'No publisher found for ID {VALUE}',
     },
   },
-  publisherName: {
-    type: String,
+  templateId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('template').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No template found for ID {VALUE}',
+    },
+  },
+  topicId: {
+    type: Schema.Types.ObjectId,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('topic').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No template found for ID {VALUE}',
+    },
   },
 }, { timestamps: true });
 
@@ -32,8 +60,24 @@ schema.pre('save', async function setPublisherName() {
   }
 });
 
+schema.pre('save', async function setTemplateName() {
+  if (this.isModified('templateId') || !this.templateName) {
+    const template = await connection.model('template').findOne({ _id: this.templateId }, { name: 1 });
+    this.templateName = template.name;
+  }
+});
+
+schema.pre('save', async function setTopicName() {
+  if (this.isModified('topicId') && this.topicId) {
+    const topic = await connection.model('topic').findOne({ _id: this.topicId }, { name: 1 });
+    this.topicName = topic.name;
+  }
+});
+
 setEntityFields(schema, 'name');
 setEntityFields(schema, 'publisherName');
+setEntityFields(schema, 'topicName');
+setEntityFields(schema, 'templateName');
 applyElasticPlugin(schema, 'placements');
 
 schema.index({ publisherId: 1, name: 1 }, { unique: true });
