090e844aacd3dec68c858868cdd73adee3c26c24
Jacob Bare <jacob@limit0.io>
2018-07-19 13:49:51 -0500

    Apply plugins to contact schema

--- src/schema/contact.js
+++ src/schema/contact.js
@@ -1,6 +1,11 @@
 const { Schema } = require('mongoose');
 const validator = require('validator');
 const { applyElasticPlugin, setEntityFields } = require('../elastic/mongoose');
+const {
+  paginablePlugin,
+  repositoryPlugin,
+  searchablePlugin,
+} = require('../plugins');
 
 const schema = new Schema({
   email: {
@@ -50,13 +55,30 @@ const schema = new Schema({
   timestamps: true,
 });
 
+setEntityFields(schema, 'name');
+applyElasticPlugin(schema, 'contacts');
+
+schema.plugin(repositoryPlugin);
+schema.plugin(paginablePlugin);
+schema.plugin(searchablePlugin, {
+  fieldNames: ['name'],
+  beforeSearch: (query, phrase) => {
+    const { should } = query.bool;
+    should.push({ match: { email: { query: phrase, boost: 5 } } });
+    should.push({ match: { 'email.edge': { query: phrase, operator: 'and', boost: 2 } } });
+    should.push({ match: { 'email.edge': { query: phrase, boost: 1 } } });
+  },
+  beforeAutocomplte: (query, phrase) => {
+    const { should } = query.bool;
+    should.push({ match: { 'email.edge': { query: phrase, operator: 'and', boost: 2 } } });
+    should.push({ match: { 'email.edge': { query: phrase, boost: 1 } } });
+  },
+});
+
 schema.pre('save', function setName(next) {
   this.name = `${this.givenName} ${this.familyName}`;
   next();
 });
-
-setEntityFields(schema, 'name');
-applyElasticPlugin(schema, 'contacts');
 
 schema.index({ name: 1, _id: 1 }, { unique: true });
 schema.index({ name: -1, _id: -1 }, { unique: true });
