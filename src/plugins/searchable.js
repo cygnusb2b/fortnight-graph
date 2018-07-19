2e3d07c7cca833f7692d88ed33c8db50408fee6b
Jacob Bare <jacob@limit0.io>
2018-07-19 13:49:08 -0500

    Add before search and autocomplete callbacks

--- src/plugins/searchable.js
+++ src/plugins/searchable.js
@@ -4,16 +4,22 @@ const {
   paginateSearch,
 } = require('../elastic/utils');
 
-module.exports = function searchablePlugin(schema, { fieldNames = [] } = {}) {
+module.exports = function searchablePlugin(schema, {
+  fieldNames = [],
+  beforeSearch,
+  beforeAutocomplete,
+} = {}) {
   // eslint-disable-next-line no-param-reassign
   schema.statics.search = function search(phrase, { pagination } = {}) {
     const query = buildEntityNameQuery(phrase, fieldNames);
+    if (typeof beforeSearch === 'function') beforeSearch(query, phrase);
     return paginateSearch(this, phrase, query, { pagination });
   };
 
   // eslint-disable-next-line no-param-reassign
   schema.statics.autocomplete = function autocomplete(phrase, { pagination } = {}) {
     const query = buildEntityAutocomplete(phrase, fieldNames);
+    if (typeof beforeAutocomplete === 'function') beforeAutocomplete(query, phrase);
     return paginateSearch(this, phrase, query, { pagination });
   };
 };
