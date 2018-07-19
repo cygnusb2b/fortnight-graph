dcb2ee80c681b541756be811a32ec82cffe4bd48
Jacob Bare <jacob@limit0.io>
2018-07-19 12:54:15 -0500

    Add plugin index

---
+++ src/plugins/index.js
@@ -0,0 +1,15 @@
+const imagePlugin = require('./image');
+const analyticsPlugin = require('./analytics');
+const contactPlugin = require('./notify');
+const paginablePlugin = require('./paginable');
+const pushId = require('./push-id');
+const repositoryPlugin = require('./repository');
+
+module.exports = {
+  imagePlugin,
+  analyticsPlugin,
+  contactPlugin,
+  paginablePlugin,
+  pushId,
+  repositoryPlugin,
+};
