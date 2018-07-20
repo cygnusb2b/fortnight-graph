4d8702b948f206f1d33d17423f52048dfe784f12
Jacob Bare <jacob@limit0.io>
2018-07-20 10:27:02 -0500

    Apply new plugins to topic schema

--- src/schema/topic.js
+++ src/schema/topic.js
@@ -1,6 +1,11 @@
 const { Schema } = require('mongoose');
 const connection = require('../connections/mongoose/instance');
 const { applyElasticPlugin, setEntityFields } = require('../elastic/mongoose');
+const {
+  paginablePlugin,
+  repositoryPlugin,
+  searchablePlugin,
+} = require('../plugins');
 
 const schema = new Schema({
   name: {
@@ -27,6 +32,14 @@ const schema = new Schema({
   },
 }, { timestamps: true });
 
+setEntityFields(schema, 'name');
+setEntityFields(schema, 'publisherName');
+applyElasticPlugin(schema, 'topics');
+
+schema.plugin(repositoryPlugin);
+schema.plugin(paginablePlugin);
+schema.plugin(searchablePlugin, { fieldNames: ['name', 'publisherName'] });
+
 schema.pre('save', async function setPublisherName() {
   if (this.isModified('publisherId') || !this.publisherName) {
     const publisher = await connection.model('publisher').findOne({ _id: this.publisherId }, { name: 1 });
@@ -47,10 +60,6 @@ schema.pre('save', async function updatePlacements() {
     });
   }
 });
-
-setEntityFields(schema, 'name');
-setEntityFields(schema, 'publisherName');
-applyElasticPlugin(schema, 'topics');
 
 schema.index({ publisherId: 1, name: 1 }, { unique: true });
 schema.index({ name: 1, _id: 1 }, { unique: true });
