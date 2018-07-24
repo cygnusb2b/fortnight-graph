4c9572d0d47233cbcafec8aaf5bd41e037942deb
Jacob Bare <jacob@limit0.io>
2018-07-24 14:41:01 -0500

    Convert searchable functions to use .static

--- src/plugins/searchable.js
+++ src/plugins/searchable.js
@@ -9,17 +9,15 @@ module.exports = function searchablePlugin(schema, {
   beforeSearch,
   beforeAutocomplete,
 } = {}) {
-  // eslint-disable-next-line no-param-reassign
-  schema.statics.search = function search(phrase, { pagination, postFilter } = {}) {
+  schema.static('search', function search(phrase, { pagination, postFilter } = {}) {
     const query = buildEntityNameQuery(phrase, fieldNames);
     if (typeof beforeSearch === 'function') beforeSearch(query, phrase);
     return paginateSearch(this, phrase, query, { pagination, postFilter });
-  };
+  });
 
-  // eslint-disable-next-line no-param-reassign
-  schema.statics.autocomplete = function autocomplete(phrase, { pagination, postFilter } = {}) {
+  schema.static('autocomplete', function autocomplete(phrase, { pagination, postFilter } = {}) {
     const query = buildEntityAutocomplete(phrase, fieldNames);
     if (typeof beforeAutocomplete === 'function') beforeAutocomplete(query, phrase);
     return paginateSearch(this, phrase, query, { pagination, postFilter });
-  };
+  });
 };
