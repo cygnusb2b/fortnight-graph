b0f877ba1f6769f999af10e72ed4dafcf011e9d5
Josh Worden <solocommand@gmail.com>
2018-07-30 09:25:19 -0500

    Wrap jwt auth in another promise

--- src/connections/google/auth.js
+++ src/connections/google/auth.js
@@ -8,9 +8,9 @@ const { client_email, private_key } = require('../../env').GOOGLE_APPLICATION_CR
  */
 module.exports = async (endpoints = []) => {
   const client = new JWT(client_email, null, private_key, endpoints);
-  await client.authorize((err) => {
-    if (err) return Promise.reject(err);
-    return Promise.resolve();
+  const authorize = new Promise((resolve, reject) => {
+    client.authorize(err => err ? reject(err) : resolve());
   });
+  await authorize;
   return client;
 };
