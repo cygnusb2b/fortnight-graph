3a84835b01aa2827d1294b028b857ac7a4415567
Jacob Bare <jacob@limit0.io>
2018-08-01 13:15:28 -0500

    Add updateAccount mutation

--- src/graph/resolvers/account.js
+++ src/graph/resolvers/account.js
@@ -1,7 +1,27 @@
+const Account = require('../../models/account');
 const AccountService = require('../../services/account');
 
 module.exports = {
+  /**
+   *
+   */
   Query: {
     account: () => AccountService.retrieve(),
+  },
+
+  /**
+   *
+   */
+  Mutation: {
+    /**
+     *
+     */
+    updateAccount: async (root, { input }, { auth }) => {
+      auth.checkAdmin();
+      const { id, payload } = input;
+      const account = await Account.strictFindById(id);
+      account.set(payload);
+      return account.save();
+    },
   },
 };
