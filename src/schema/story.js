180322443bd21f0994b68cc7daa8b962f54ed06f
Jacob Bare <jacob@limit0.io>
2018-08-01 12:28:24 -0500

    Add virtual url property to story schema

--- src/schema/story.js
+++ src/schema/story.js
@@ -1,5 +1,7 @@
 const { Schema } = require('mongoose');
 const slug = require('slug');
+const accountService = require('../services/account');
+const storyUrl = require('../utils/story-url');
 const connection = require('../connections/mongoose/instance');
 const { applyElasticPlugin, setEntityFields } = require('../elastic/mongoose');
 const {
@@ -63,6 +65,10 @@ schema.plugin(imagePlugin, { fieldName: 'imageIds', multiple: true });
 schema.plugin(repositoryPlugin);
 schema.plugin(paginablePlugin);
 schema.plugin(searchablePlugin, { fieldNames: ['title', 'advertiserName'] });
+
+schema.virtual('url').get(function getUrl() {
+  return storyUrl(accountService.getStoryHost(), this.id);
+});
 
 schema.virtual('slug').get(function getSlug() {
   return slug(this.title).toLowerCase();
