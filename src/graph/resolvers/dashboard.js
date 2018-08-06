21ddc7c6bb55c4f3318a70c4c1d7fc5df5277603
Jacob Bare <jacob@limit0.io>
2018-08-06 10:40:58 -0500

    Add dailyFallbackMetrics query

--- src/graph/resolvers/dashboard.js
+++ src/graph/resolvers/dashboard.js
@@ -71,5 +71,71 @@ module.exports = {
         ctr: 0,
       };
     },
+
+    /**
+     *
+     */
+    dailyFallbackMetrics: async (root, { day }, { auth }) => {
+      auth.check();
+      const end = moment(day).add(24, 'hours').toDate();
+
+      const pipeline = [];
+      pipeline.push({
+        $match: {
+          e: { $in: ['view-js', 'click-js'] },
+          cid: { $exists: false },
+          d: { $gte: day, $lt: end },
+        },
+      });
+      pipeline.push({
+        $project: {
+          pid: 1,
+          view: { $cond: [{ $eq: ['$e', 'view-js'] }, 1, 0] },
+          click: { $cond: [{ $eq: ['$e', 'click-js'] }, 1, 0] },
+        },
+      });
+      pipeline.push({
+        $group: {
+          _id: '$pid',
+          views: { $sum: '$view' },
+          clicks: { $sum: '$click' },
+        },
+      });
+      pipeline.push({
+        $group: {
+          _id: null,
+          placements: { $push: '$_id' },
+          views: { $sum: '$views' },
+          clicks: { $sum: '$clicks' },
+        },
+      });
+      pipeline.push({
+        $project: {
+          _id: 0,
+          placements: { $size: '$placements' },
+          views: 1,
+          clicks: 1,
+          ctr: {
+            $cond: {
+              if: {
+                $eq: ['$views', 0],
+              },
+              then: 0,
+              else: {
+                $divide: ['$clicks', '$views'],
+              },
+            },
+          },
+        },
+      });
+
+      const result = await AnalyticsEvent.aggregate(pipeline);
+      return result[0] ? result[0] : {
+        placements: 0,
+        views: 0,
+        clicks: 0,
+        ctr: 0,
+      };
+    },
   },
 };
