94c90a1de1647510ffd9e7324cc6d553127163e2
Jacob Bare <jacob@limit0.io>
2018-08-06 14:07:03 -0500

    Ensure all campaigns are included in daily report
    Accounts for campaigns that may not have any activity

--- src/graph/resolvers/dashboard.js
+++ src/graph/resolvers/dashboard.js
@@ -1,5 +1,7 @@
 const moment = require('moment');
 const AnalyticsEvent = require('../../models/analytics/event');
+const Campaign = require('../../models/campaign');
+const campaignDelivery = require('../../services/campaign-delivery');
 
 module.exports = {
   /**
@@ -11,13 +13,24 @@ module.exports = {
      */
     dailyCampaignMetrics: async (root, { day }, { auth }) => {
       auth.check();
+      const defaultResult = {
+        campaigns: 0,
+        views: 0,
+        clicks: 0,
+        ctr: 0,
+      };
+
+      const campaignCriteria = campaignDelivery.getDefaultCampaignCriteria();
+      const campaigns = await Campaign.find(campaignCriteria, { _id: 1 });
+      if (!campaigns.length) return defaultResult;
+
       const end = moment(day).add(24, 'hours').toDate();
 
       const pipeline = [];
       pipeline.push({
         $match: {
           e: { $in: ['view-js', 'click-js'] },
-          cid: { $exists: true },
+          cid: { $in: campaigns.map(c => c._id) },
           d: { $gte: day, $lt: end },
         },
       });
@@ -64,12 +77,9 @@ module.exports = {
       });
 
       const result = await AnalyticsEvent.aggregate(pipeline);
-      return result[0] ? result[0] : {
-        campaigns: 0,
-        views: 0,
-        clicks: 0,
-        ctr: 0,
-      };
+      const data = result[0] ? result[0] : defaultResult;
+      if (data.campaigns < campaigns.length) data.campaigns = campaigns.length;
+      return data;
     },
 
     /**
