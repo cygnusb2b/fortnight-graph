d87099c2f33ae9371efa90eb6747a9c8816efdde
Jacob Bare <jacob@limit0.io>
2018-08-13 14:54:51 -0500

    Ensure path is returned from story

--- src/services/campaign-delivery.js
+++ src/services/campaign-delivery.js
@@ -109,7 +109,7 @@ module.exports = {
     // Campaign is linked to a story, generate using publiser or account host.
     const publisher = await Publisher.findById(publisherId, { domainName: 1 });
     const account = await accountService.retrieve();
-    const path = await Story.findById(storyId, { body: 0 });
+    const { path } = await Story.findById(storyId, { body: 0 });
     return storyUrl(publisher.customUri || account.storyUri, path, { pubid: publisherId });
   },
 
