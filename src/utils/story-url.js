da52fb7fcc24c503864649188c57b7239036f790
Jacob Bare <jacob@limit0.io>
2018-08-13 14:36:57 -0500

    Update story url util to support params

--- src/utils/story-url.js
+++ src/utils/story-url.js
@@ -1,1 +1,9 @@
-module.exports = (uri, path) => `${uri}/${path}`;
+const querystring = require('querystring');
+
+module.exports = (uri, path, params) => {
+  const url = `${uri.replace(/\/+$/g, '')}/${path.replace(/^\/+/g, '').replace(/\/+$/g, '')}`;
+  if (params && typeof params === 'object') {
+    return `${url}/?${querystring.stringify(params)}`;
+  }
+  return url;
+};
