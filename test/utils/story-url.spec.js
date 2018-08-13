505f6ed1f41164548f4a3b0d2b6f7dfe3f9cc688
Jacob Bare <jacob@limit0.io>
2018-08-13 14:38:49 -0500

    Add story url test for non-object params

--- test/utils/story-url.spec.js
+++ test/utils/story-url.spec.js
@@ -10,11 +10,14 @@ describe('utils/story-url', function() {
   it('should strip slashes properly', async function() {
     expect(storyUrl('https://www.google.com//', '//foo/bar/')).to.equal('https://www.google.com/foo/bar');
   });
-  it('should set query paramters', async function() {
+  it('should set query parameters', async function() {
     const params = {
       foo: 'bar',
       baz: true,
     }
     expect(storyUrl('https://www.google.com', 'foo/bar//', params)).to.equal('https://www.google.com/foo/bar/?foo=bar&baz=true');
+  });
+  it('should not set the parameters when `params` is not an object.', async function() {
+    expect(storyUrl('https://www.google.com', 'foo/bar', '1234')).to.equal('https://www.google.com/foo/bar');
   });
 });
