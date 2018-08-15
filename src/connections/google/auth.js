a1ab6242b58f19d17a059418a22d8b256f4fec0c
Jacob Bare <jacob@limit0.io>
2018-08-15 08:18:45 -0500

    Remove redudant await in google auth

--- src/connections/google/auth.js
+++ src/connections/google/auth.js
@@ -6,14 +6,12 @@ const { client_email, private_key } = env.GOOGLE_APPLICATION_CREDENTIALS;
 
 /**
  *
+ * @async
  * @param {Array} endpoints Google API endpoints to be authorized for this client
- * @returns JWT
  */
 module.exports = async (endpoints = []) => {
   const client = new JWT(client_email, null, private_key, endpoints);
-  const authorize = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     client.authorize(err => (err ? reject(err) : resolve()));
-  });
-  await authorize;
-  return client;
+  }).then(() => client);
 };
