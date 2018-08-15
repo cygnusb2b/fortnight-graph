bb3cd31a72334fb831a84f9fcb2372791519b5cd
Jacob Bare <jacob@limit0.io>
2018-08-15 10:52:51 -0500

    Consolidate common metrics and story filter

--- src/services/google-analytics.js
+++ src/services/google-analytics.js
@@ -6,7 +6,6 @@ const { isArray } = Array;
 const { GA_VIEW_ID } = env;
 
 let conn;
-
 module.exports = {
   async connect() {
     if (!conn) {
@@ -28,32 +27,14 @@ module.exports = {
   async storyReport(storyId, { startDate, endDate }) {
     if (!storyId) throw new Error('No story ID was provided.');
     const dateRanges = [{ startDate, endDate }];
-    const metrics = [
-      { expression: 'ga:pageviews' },
-      { expression: 'ga:uniquePageviews' },
-      { expression: 'ga:sessions' },
-      { expression: 'ga:users' },
-      { expression: 'ga:avgSessionDuration' },
-      { expression: 'ga:bounceRate' },
-      { expression: 'ga:timeOnPage' },
-      { expression: 'ga:avgTimeOnPage' },
-    ];
     const dimensionFilterClauses = [
-      {
-        filters: [
-          {
-            dimensionName: 'ga:dimension2',
-            operator: 'EXACT',
-            expressions: [storyId],
-          },
-        ],
-      },
+      { filters: [this.getStoryFilter(storyId)] },
     ];
 
     const request = {
       viewId: GA_VIEW_ID,
       dateRanges,
-      metrics,
+      metrics: this.getStandardMetrics(),
       dimensionFilterClauses,
       includeEmptyRows: true,
       hideTotals: true,
@@ -94,5 +75,26 @@ module.exports = {
       requestBody: { reportRequests },
     });
     return res.data;
+  },
+
+  getStoryFilter(storyId) {
+    return {
+      dimensionName: 'ga:dimension2',
+      operator: 'EXACT',
+      expressions: [storyId],
+    };
+  },
+
+  getStandardMetrics() {
+    return [
+      { expression: 'ga:pageviews' },
+      { expression: 'ga:uniquePageviews' },
+      { expression: 'ga:sessions' },
+      { expression: 'ga:users' },
+      { expression: 'ga:avgSessionDuration' },
+      { expression: 'ga:bounceRate' },
+      { expression: 'ga:timeOnPage' },
+      { expression: 'ga:avgTimeOnPage' },
+    ];
   },
 };
