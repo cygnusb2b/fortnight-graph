cda77d20cf7beefc86d580976ca2afff464327d0
Jacob Bare <jacob@limit0.io>
2018-12-14 10:49:52 -0600

    Move start/stop promises into own files

--- src/index.js
+++ src/index.js
@@ -2,32 +2,20 @@ const http = require('http');
 const { createTerminus } = require('@godaddy/terminus');
 require('./newrelic');
 const app = require('./app');
-const elastic = require('./elastic');
 const env = require('./env');
 const health = require('./health');
-const initElastic = require('./elastic/init');
-const mongoose = require('./connections/mongoose');
 const pkg = require('../package.json');
-const redis = require('./redis');
 const start = require('./start');
 const stop = require('./stop');
 const { write } = require('./output');
 
-const { PORT, ELASTIC_HOST, ELASTIC_INDEX_RECREATE } = env;
+const { PORT } = env;
 
 const server = http.createServer(app);
 
 const boot = async () => {
   // Start any services that need to connect before the web server listens...
-  await Promise.all([
-    start(mongoose.core, 'MongoDB core', m => m.client.s.url),
-    start(mongoose.instance, 'MongoDB tenant', m => m.client.s.url),
-    start(elastic.connect().then(() => initElastic(elastic, ELASTIC_INDEX_RECREATE)), 'ElasticSearch', ELASTIC_HOST),
-    start(new Promise((resolve, reject) => {
-      redis.on('connect', resolve);
-      redis.on('error', reject);
-    }), 'Redis', () => redis.options.url),
-  ]);
+  await start();
 
   // Create the terminus instance for health checks and graceful shutdown...
   createTerminus(server, {
@@ -36,24 +24,13 @@ const boot = async () => {
     healthChecks: { '/_health': health() },
     onSignal: () => {
       write('> Cleaning up...');
-      return Promise.all([
-        stop(mongoose.core.close(), 'MongoDB core'),
-        stop(mongoose.instance.close(), 'MongoDB instance'),
-        stop(new Promise((resolve, reject) => {
-          redis.on('end', resolve);
-          redis.on('error', reject);
-          redis.quit();
-        }), 'Redis'),
-        stop(elastic.disconnect(), 'ElasticSearch'),
-      ]).catch(e => write('> CLEANUP ERRORS:', e));
+      return stop().catch(e => write('> CLEANUP ERRORS:', e));
     },
     onShutdown: () => write('> Cleanup finished. Shutting down.'),
   });
 
   // Run the web server.
-  server.listen(PORT, () => {
-    write(`> Ready on http://0.0.0.0:${PORT}`);
-  });
+  server.listen(PORT, () => write(`> Ready on http://0.0.0.0:${PORT}`));
 };
 
 // Simulate future NodeJS behavior by throwing unhandled Promise rejections.
--- src/start.js
+++ src/start.js
@@ -1,6 +1,13 @@
+const elastic = require('./elastic');
+const env = require('./env');
+const initElastic = require('./elastic/init');
+const mongoose = require('./connections/mongoose');
+const redis = require('./redis');
 const { write } = require('./output');
 
-module.exports = (promise, name, url) => {
+const { ELASTIC_HOST, ELASTIC_INDEX_RECREATE } = env;
+
+const start = (promise, name, url) => {
   write(`> Connecting to ${name}...`);
   return promise.then((r) => {
     const u = typeof url === 'function' ? url(r) : url;
@@ -8,3 +15,13 @@ module.exports = (promise, name, url) => {
     return r;
   });
 };
+
+module.exports = () => Promise.all([
+  start(mongoose.core, 'MongoDB core', m => m.client.s.url),
+  start(mongoose.instance, 'MongoDB tenant', m => m.client.s.url),
+  start(elastic.connect().then(() => initElastic(elastic, ELASTIC_INDEX_RECREATE)), 'ElasticSearch', ELASTIC_HOST),
+  start(new Promise((resolve, reject) => {
+    redis.on('connect', resolve);
+    redis.on('error', reject);
+  }), 'Redis', () => redis.options.url),
+]);
