b1d3f837b7076cbbbb06a13f5b361a4659aa4e63
Josh Worden <josh@parameter1.com>
2021-04-02 13:45:34 -0500

    Update story-url utility to use story directly

--- src/utils/story-url.js
+++ src/utils/story-url.js
@@ -1,6 +1,18 @@
 const querystring = require('querystring');
+const connection = require('../connections/mongoose/instance');
+const accountService = require('../services/account');
 
-module.exports = (uri, path, params) => {
+module.exports = async (story, params) => {
+  const [
+    account,
+    publisher,
+    path,
+  ] = await Promise.all([
+    accountService.retrieve(),
+    connection.model('publisher').findById(story.publisherId),
+    story.getPath(),
+  ]);
+  const uri = publisher.customUri || account.storyUri;
   const url = `${uri.replace(/\/+$/g, '')}/${path.replace(/^\/+/g, '').replace(/\/+$/g, '')}`;
   if (params && typeof params === 'object') {
     return `${url}/?${querystring.stringify(params)}`;
